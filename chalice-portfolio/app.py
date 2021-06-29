6341fdfc0fc5de937aba404a17c5952c6fb1f3bf
vbalasu <vbalasu@gmail.com>
2021-06-29 14:51:01 -0700

    Added chalice-portfolio

---
+++ chalice-portfolio/app.py
@@ -0,0 +1,51 @@
+from chalice import Chalice, Response
+import boto3
+
+app = Chalice(app_name='chalice-portfolio')
+
+
+@app.route('/get/{user}/{cid}')
+def get(user, cid):
+    url = f's3://cloudmatica/portfolio/{user}/{cid}'
+    print('get', url)
+    app.api.binary_types.append("text/csv")
+    s3 = boto3.client('s3')
+    s3.download_file('cloudmatica', f'portfolio/{user}/{cid}', '/tmp/portfolio.csv')
+    with open('/tmp/portfolio.csv', 'rb') as f:
+        body = f.read()
+    return Response(body=body, headers={'Content-Type': 'text/csv', 'Content-Disposition': 'attachment; filename=portfolio.csv'})
+
+# USAGE: http PUT http://127.0.0.1:8000/put/vbalasu.kb@gmail.com/portfolio @portfolio.csv
+@app.route('/put/{user}/{cid}', methods=['PUT'], content_types=['text/csv'])
+def put(user, cid):
+    url = f's3://cloudmatica/portfolio/{user}/{cid}'
+    print('put', url)
+    payload = app.current_request.raw_body
+    s3 = boto3.client('s3')
+    with open('/tmp/portfolio.csv', 'wb') as f:
+        f.write(payload)
+    s3.upload_file('/tmp/portfolio.csv', 'cloudmatica', f'portfolio/{user}/{cid}')
+    return {'success': True}
+
+
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
