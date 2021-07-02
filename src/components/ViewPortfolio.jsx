9818a29bd1ee1fc5d91a0305abcc372f8e9cd07b
vbalasu <vbalasu@gmail.com>
2021-07-02 15:02:11 -0700

    Created 4 states

--- src/components/ViewPortfolio.jsx
+++ src/components/ViewPortfolio.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { main } from './utility';
 
 class ViewPortfolio extends Component {
-  state = { 
-    mode: 'view'
-   };
-  editMode = () => {
-    console.log('Edit mode');
-    this.setState({mode: 'edit'});
+  validStates = ['1-ready', '2-waitingForTokenGen', '3-enterToken', '4-sendingData']
+  state = {
+    status: '1-ready'
+  }
+  saveButton = async () => {
+    console.log('Save button');
+    if(this.state.status === '1-ready') {
+      this.setState({status: '2-waitingForTokenGen'});
+      var response = await fetch(`https://email-authentication.cloudmatica.com/generate/${this.props.user}`);
+      var result = await response.json();
+      this.setState({status: '3-enterToken'});
+      console.log(result);  
+    }
+    else if (this.state.status === '3-enterToken') {
+      this.setState({status: '4-sendingData'});
+      console.log('Sending data');
+      this.setState({status: '1-ready'})
+    }
   }
 
   componentDidMount() {
@@ -26,9 +38,18 @@ class ViewPortfolio extends Component {
         <div style={{height:"400px"}}>
           <div id="hot1"></div>
         </div>
-        <button className="btn btn-primary" onClick={this.editMode}>Save</button>
+        <span>
+          { this.state.status === '3-enterToken' ? <input className="m-2 col-sm-5" type="text" id="token" name="token" placeholder="OTP (Check your email)" /> : <div></div> }
+          { this.state.status === '2-waitingForTokenGen' || this.state.status === '4-sendingData' ? <button class="btn btn-primary" type="button" disabled>
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            Please Wait...
+            </button>
+          : <button className="btn btn-primary" onClick={this.saveButton}>Save</button>
+          }
+        </span>
+
         <br /><br />
-        <h2>Crypto Prices</h2>
+        <h2>Coingecko Prices</h2>
         <div style={{height:"400px"}}>
           <div id="hot2"></div>
         </div>
